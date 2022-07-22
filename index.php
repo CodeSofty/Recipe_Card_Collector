7f7a05c22c49dc5bee2048b179b65595a270d245
CodeSoftly <eclittlejohn@yahoo.com>
2022-07-21 22:39:26 -0500

    commit

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+
 //Require Models (database, API, and class files)
 require_once('config/database.php');
 require('model/recipe_db.php');
