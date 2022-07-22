eb77be13165e6d3b1fe7d3a2449ec24bc8e94d8e
CodeSoftly <eclittlejohn@yahoo.com>
2022-07-21 22:19:32 -0500

    commit

--- aws/api/upload.php
+++ aws/api/upload.php
@@ -68,7 +68,9 @@ $s3Client = $sdk->createS3();
         'Bucket' => "crudappbucket",
         'Body'   => "this is the body!",
         'Key' =>  $file_name,
-        'SourceFile' => 'images/'.$file_name,
+        'SourceFile' => __DIR__ . "/images/".$file_name,
+
+        
     ]);
 // Echo Any Error Messages
 } catch (S3Exception $e) {
