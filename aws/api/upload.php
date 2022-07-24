ef39d2b0d0fa105c568da85adfe5d253731a458e
CodeSoftly <eclittlejohn@yahoo.com>
2022-07-23 19:09:37 -0500

    commit

--- aws/api/upload.php
+++ aws/api/upload.php
@@ -45,34 +45,35 @@ function uploadFiletoS3($file_name){
 
     if(isset($file_name)) {
 
+
 // Shared S3 Configuration
-// try {
-//     $sharedConfig = [
-//         'region' => 'us-east-2',
-//         'version' => 'latest',
-//         'credentials' => array (
-//             'key' => $_ENV['AWS_ACCESS_KEY_ID'],
-//             'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
-//     ),
-//     ];
+try {
+    $sharedConfig = [
+        'region' => 'us-east-2',
+        'version' => 'latest',
+        'credentials' => array (
+            'key' => $_ENV['AWS_ACCESS_KEY_ID'],
+            'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
+    ),
+    ];
 
-// // Give the SDK the configuration for connection
-// $sdk = new Aws\Sdk($sharedConfig);
-// // Create an S3 Client
-// $s3Client = $sdk->createS3();
+// Give the SDK the configuration for connection
+$sdk = new Aws\Sdk($sharedConfig);
+// Create an S3 Client
+$s3Client = $sdk->createS3();
 
 
-// // Use S3 client to upload image stored in images folder to the S3 Bucket
-//     $result = $s3Client->putObject([
-//         'Bucket' => "crudappbucket",
-//         'Body'   => "this is the body!",
-//         'Key' =>  $file_name,
-//         'SourceFile' => 'aws/api/images/'.$file_name,
-//     ]);
-// // Echo Any Error Messages
-// } catch (S3Exception $e) {
-//     echo "An Error With AWS Occured With Image Upload:" .  $e->getMessage() . "\n";
-// };
+// Use S3 client to upload image stored in images folder to the S3 Bucket
+    $result = $s3Client->putObject([
+        'Bucket' => "crudappbucket",
+        'Body'   => "this is the body!",
+        'Key' =>  $file_name,
+        'SourceFile' => 'aws/api/images/'.$file_name,
+    ]);
+// Echo Any Error Messages
+} catch (S3Exception $e) {
+    echo "An Error With AWS Occured With Image Upload:" .  $e->getMessage() . "\n";
+};
 }
 }
 ?>
