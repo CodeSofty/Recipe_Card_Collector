1a640c5d9d9f93880ccf9211e7da19aa4fb7bf47
CodeSoftly <eclittlejohn@yahoo.com>
2022-07-24 23:19:05 -0500

    commit

--- aws/api/edit.php
+++ aws/api/edit.php
@@ -59,36 +59,35 @@ function updateFiletoS3($file_name){
 
     if(isset($file_name)) {
         echo "<script type='text/javascript'>alert('This File is before updating');</script>"; 
-        uploadFiletoS3($file_name);
 
 // // Shared S3 Configuration
-// try {
-//     $sharedConfig = [
-//         'region' => 'us-east-2',
-//         'version' => 'latest',
-//         'credentials' => array (
-//             'key' => $_ENV['AWS_ACCESS_KEY_ID'],
-//             'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
-//     ),
-//     ];
+try {
+    $sharedConfig = [
+        'region' => 'us-east-2',
+        'version' => 'latest',
+        'credentials' => array (
+            'key' => $_ENV['AWS_ACCESS_KEY_ID'],
+            'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
+    ),
+    ];
 
-// // Give the SDK the configuration for connection
-// $sdk = new Aws\Sdk($sharedConfig);
-// // Create an S3 Client
-// $s3Client = $sdk->createS3();
+// Give the SDK the configuration for connection
+$sdk = new Aws\Sdk($sharedConfig);
+// Create an S3 Client
+$s3Client = $sdk->createS3();
 
 
-// // Use S3 client to upload image stored in images folder to the S3 Bucket
-//     $result = $s3Client->putObject([
-//         'Bucket' => "crudappbucket",
-//         'Body'   => "this is the body!",
-//         'Key' =>  $file_name,
-//         'SourceFile' =>  __DIR__ .'/images/'.$file_name,
-//     ]);
-// // Echo Any Error Messages
-// } catch (S3Exception $e) {
-//     echo "An Error Occured With Image Upload:" .  $e->getMessage() . "\n";
-// };
+// Use S3 client to upload image stored in images folder to the S3 Bucket
+    $result = $s3Client->putObject([
+        'Bucket' => "crudappbucket",
+        'Body'   => "this is the body!",
+        'Key' =>  $file_name,
+        'SourceFile' =>  __DIR__ .'/images/'.$file_name,
+    ]);
+// Echo Any Error Messages
+} catch (S3Exception $e) {
+    echo "An Error Occured With Image Upload:" .  $e->getMessage() . "\n";
+};
 }
 };
 
