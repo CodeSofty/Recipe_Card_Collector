0f1334ac657b1312d61f499f74a305343ac9f42f
CodeSoftly <eclittlejohn@yahoo.com>
2022-07-24 23:53:42 -0500

    commit

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+// ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+ini_get('upload_max_filesize');
 
 
 //Require Models (database, API, and class files)
