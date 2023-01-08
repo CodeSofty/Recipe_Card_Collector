ad60cfeabad180b74704f3dcf2c82ae6897b268a
CodeSoftly <eclittlejohn@yahoo.com>
2023-01-08 13:50:11 -0600

    commit

--- config/database.php
+++ config/database.php
@@ -2,12 +2,21 @@
 
 
 class Database {
-    //DB Params
-    private $host = 'us-cdbr-east-06.cleardb.net';
-    private $db_name = 'heroku_ae9138c33b4413d';
-    private $username = 'b8aa0663acecb1';
-    private $pass =  'a7faeb32';
-    private $conn;
+    // //DB Params
+    // private $host = 'us-cdbr-east-06.cleardb.net';
+    // private $db_name = 'heroku_ae9138c33b4413d';
+    // private $username = 'b8aa0663acecb1';
+    // private $pass =  'a7faeb32';
+    // private $conn;
+
+        //DB Params
+        private $host = 'crud-database-do-user-13269986-0.b.db.ondigitalocean.com';
+        private $db_name = 'defaultdb';
+        private $username = 'doadmin';
+        private $pass =  'AVNS_o0g1qS2iFRLoy9UbqPd';
+        private $port = '25060';
+        private $sslmode = 'REQUIRED';
+        private $conn;
     
 
     //DB Connect
